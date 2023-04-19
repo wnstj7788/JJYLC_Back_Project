a7166c2f9491f989d9814cd03c9ddce15ec2c629
jjunehee <jjhjjh1159@gmail.com>
2023-04-19 20:37:01 +0900

    .gitignore 수정

--- src/main/java/org/ssafy/shopping/backend/controller/ItemController.java
+++ src/main/java/org/ssafy/shopping/backend/controller/ItemController.java
@@ -34,31 +34,29 @@ public class ItemController {
     public String uploadFile(@RequestParam("file") MultipartFile file, @RequestParam("name") String name, @RequestParam("price") String price, @RequestParam("discountPer") String discountPer) {
     try {
         // 파일 저장 디렉토리 경로 설정
-        // String uploadDir = "/uploads/";
-        String uploadDir = "src/main/resources/static/uploads" + File.separatorChar;
+        
+        
         // 파일 이름
         String fileName = StringUtils.cleanPath(file.getOriginalFilename());
 
         // 파일 저장 경로 생성
-        System.out.println(uploadDir);
+        String uploadDir = "src/main/resources/static/uploads/";
         Path uploadPath = Paths.get(uploadDir);
-        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-        System.out.println(uploadPath.toString());
-        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+        
         if (!Files.exists(uploadPath)) {
             Files.createDirectories(uploadPath);
         }
         
         // 파일 저장 경로와 파일 이름 결합
         Path filePath = uploadPath.resolve(fileName);
-
-        System.out.println(filePath.toString());
+        
         // 파일 저장
         Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
 
         Item item = new Item();
         item.setName(fileName);
-        item.setImgPath(filePath.toString());
+        String uploadDBpath = "uploads/";
+        item.setImgPath(uploadDBpath + fileName);
         item.setDiscountPer(discountPer);
         item.setPrice(price);
         itemRepository.save(item);
